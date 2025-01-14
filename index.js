d9e395690fa97e1ebfebe82b43ae2510b389433d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-01-14 14:20:45 +0700

    Adjust content position

--- index.js
+++ index.js
@@ -273,12 +273,14 @@ fetch('https://www.omgubuntu.co.uk/wp-json/wp/v2/posts?per_page=3&_embed=true')
     posts.forEach(post => {
         let featuredImage = post._embedded['wp:featuredmedia'] ? post._embedded['wp:featuredmedia'][0].source_url : '';
         let postTitle = post.title.rendered.substring(0, 64) + (post.title.rendered.length > 64 ? '...' : '');
-        let contentPreview = post.content.rendered.substring(0, 64) + (post.content.rendered.length > 64 ? '...' : '');
+        // let contentPreview = post.content.rendered.substring(0, 64) + (post.content.rendered.length > 64 ? '...' : '');
 
         postsHtml += `
             <div class="post">
-                <div>${featuredImage ? `<img src="${featuredImage}" alt="${post.title.rendered}" class="featured-image" />` : ''}</div>
-                <a href="${post.link}" class="post-title" target="_blank">${postTitle}</a>
+                <div class="post-content">
+                    <div>${featuredImage ? `<img src="${featuredImage}" alt="${post.title.rendered}" class="featured-image" />` : ''}</div>
+                    <a href="${post.link}" class="post-title" target="_blank">${postTitle}</a>
+                </div>
                 <div class="post-date">${new Date(post.date).toISOString().split('T')[0]}</div>
             </div>
         `;
