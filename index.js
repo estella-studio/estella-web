0b4a8b1091f8e4b6575877fb537abc97e27efc22
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-01-15 15:36:07 +0700

    Post content

--- index.js
+++ index.js
@@ -273,14 +273,13 @@ fetch('https://dev.estellastudiodev.com/wp-json/wp/v2/posts?per_page=3&_embed=tr
     posts.forEach(post => {
         let featuredImage = post._embedded['wp:featuredmedia'] ? post._embedded['wp:featuredmedia'][0].source_url : '';
         let postTitle = post.title.rendered.substring(0, 64) + (post.title.rendered.length > 64 ? '...' : '');
-        // let contentPreview = post.content.rendered.substring(0, 64) + (post.content.rendered.length > 64 ? '...' : '');
-
+        let postContent = post.content.rendered.substring(0, 32) + (post.content.rendered.length > 32 ? '...' : '');
         postsHtml += `
             <div class="post">
                 <a class="post-link" href="${post.link}" target="_blank"></a>
                 <div class="post-content">
-                    <div>${featuredImage ? `<img src="${featuredImage}" alt="${post.title.rendered}" class="featured-image" />` : ''}</div>
-                    <div class-post-title>${postTitle}</div>
+                    <div class="post-content-featured-image">${featuredImage ? `<img src="${featuredImage}" alt="${post.title.rendered}"/>` : ''}</div>
+                    <div class="post-content-title">${postTitle}</div>
                 </div>
                 <div class="post-date">${new Date(post.date).toISOString().split('T')[0]}</div>
             </div>
@@ -310,3 +309,4 @@ fetch('https://dev.estellastudiodev.com/wp-json/wp/v2/posts?per_page=3&_embed=tr
 });
 
 // <div class="post-content">${contentPreview}</div>
+// <div class="post-content-content">${postContent}</div>
